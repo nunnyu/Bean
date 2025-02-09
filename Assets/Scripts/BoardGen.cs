9a3bb11d0039b6fb353957e5d412d0f4057e60a8
Anthony Basko <antony.basko@gmail.com>
2025-02-08 23:07:23 -0500

    Visual Movement
    DFS Completed

--- Assets/Scripts/BoardGen.cs
+++ Assets/Scripts/BoardGen.cs
@@ -4,8 +4,9 @@ public class BoardGen : MonoBehaviour
 {
     public Transform road; // prefab to instantiate 
     private int[,] currentLevel;
+    public Transform goal;
     
-    public int[,] level_1 = { { 0, 1, 1 ,1, 1, 0, 1, 1, 0},
+    public int[,] level_1 = { { 0, 1, 1 ,1, 1, 0, 1, 1, 2},
                               { 0, 0, 0, 0, 1, 0, 0, 0, 0},
                               { 0, 1, 0, 1, 1, 0, 1, 1, 0},
                               { 1, 1, 0, 1, 0, 0, 1, 0, 0},
@@ -20,18 +21,20 @@ public class BoardGen : MonoBehaviour
         currentLevel = level_1;
     }
 
-    void spawnRoad(int x, int y) {
+    void spawnPrefab(Transform p, int x, int y) {
         Vector3 d = new Vector3(x, y, 0f);
         
         Quaternion r = Quaternion.identity;
-        Instantiate(road, d, r);
+        Instantiate(p, d, r);
     }
 
     void createLevel(int[,] matrix) {
         for (int i = 0; i < matrix.GetLength(0); i++) {
             for (int j = 0; j < matrix.GetLength(1); j++) {
                 if (matrix[i, j] == 0) {
-                    spawnRoad(j, matrix.GetLength(0) - 1 - i);
+                    spawnPrefab(road, j, matrix.GetLength(0) - 1 - i);
+                } else if (matrix[i, j] == 2) {
+                    spawnPrefab(goal, j, matrix.GetLength(0) - 1 - i);
                 }
             }
         }
@@ -45,5 +48,9 @@ public class BoardGen : MonoBehaviour
     public int getBoardRowCol(int row, int col)
     {
         return level_1[level_1.GetLength(0) - 1 - row, col];
+    }
+
+    public void setBoardRowCol(int row, int col, int val) {
+        level_1[level_1.GetLength(0) - 1 - row, col] = val;
     }
 }
--- Assets/Scripts/dfs.cs
+++ Assets/Scripts/dfs.cs
@@ -1,60 +1,91 @@
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class dfs : MonoBehaviour
 {
-    int[,] board;
-
-    Stack<(int, int)> stack;
-    (int, int) startPos = (0, 0);
-
-    bool finished = false;
+    private int[,] board;
+    private Stack<(int, int)> stack = new Stack<(int, int)>();
+    private bool finished = false;
     public Transform movepoint;
-        
+    public Transform road;
+    public HashSet<(int, int)> visited = new HashSet<(int, int)>();
+    BoardGen script;
+    private (int, int) pos;    
     void Start() {
-        stack = new Stack<(int, int)> ();
-        stack.Push(startPos);
-        BoardGen boardGenScript = FindObjectOfType<BoardGen>();
-        board = boardGenScript.getCurrentLevel();
+        Invoke("initialize", 0.25f); // delay everything so the board is created nicely 
+    }
+
+    void initialize() {
+        pos = (0, 2);
+        script = FindObjectOfType<BoardGen>();
+        board = script.level_1;
+        movepoint.parent = null;
+        visited.Add((pos.Item1, pos.Item2));
+        Debug.Log(board);
+
+        stack.Push(pos);
     }
 
     void Update() {
-        if (transform.position == movepoint.position) {
-            Call((((int)transform.position.x, (int)transform.position.y)));
+        if (Vector2.Distance(transform.position, movepoint.position) < 0.05f) {
+            Instantiate(road, movepoint.position, Quaternion.identity);
+            Call();
         }
-        // movepoint.position += new Vector3(1, 0);
     }
 
     // Update is called once per frame
-    void Call((int, int) pos)
+    void Call()
     {
         if (finished) {
+            Debug.Log("2");
             return;
         }
 
-        if (stack.TryPop(out pos))
-        {
-            // check if pos is the goal, if so break out and say we are finished
-            finished = true;
+        List<(int, int)> directionsList = new List<(int, int)>();
 
-            int row = pos.Item1;
-            int col = pos.Item2;
+        (int, int) current = pos;
+        if (stack.TryPop(out current))
+        {
+            movepoint.position = new Vector3(current.Item2, current.Item1);
+            if (script.getBoardRowCol(current.Item1, current.Item2) == 2) {
+                Debug.Log("1");
+                finished = true;
+                return;
+            }
+
+            // check if pos is the goal, if so break out and say we are finished
+            // finished = true;
+
+            int row = current.Item1;  
+            int col = current.Item2;
             (int, int)[] directions = { (0, 1), (1, 0), (-1, 0), (0, -1) };
-            for (int i = 3; i >= 0; i--)
+
+            for (int i = 0; i < 4; i++)  // iterating over directions
             {
                 (int, int) direction = directions[i];
-                movepoint.position += new Vector3(direction.Item1, direction.Item2);
-                Debug.Log(direction);
                 int newRow = direction.Item1 + row;
                 int newCol = direction.Item2 + col;
+
                 // Pop items onto stack
-                if (newRow >= 0 && newRow < board.Length && newCol >= 0 && newCol < board.GetLength(0) && board[newRow, newCol] != -1)
-                {
+                // Check bounds and if not visited
+                if (newRow >= 0 && newRow < board.GetLength(0) && newCol >= 0 && newCol < board.GetLength(1) && !visited.Contains((newRow, newCol)) && script.getBoardRowCol(newRow, newCol) != 1)
+                {   
                     // Mark index as visited
-                    board[newRow, newCol] = -1;
                     stack.Push((newRow, newCol));
+                    directionsList.Add((newCol, newRow));
+                    visited.Add((newRow, newCol));
                 }
             }
         }
+
+        // Debug log the list of directions (output coordinates)
+        // Debug.Log("Directions List:");
+        // int k = 1;
+        // foreach (var dir in directionsList)
+        // {
+        //     Debug.Log(k + ".     " + dir);
+        //     k++;
+        // }
     }
 }
