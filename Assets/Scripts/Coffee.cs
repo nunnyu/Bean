6488d297268ee70aa5498ff674aa97bd046100b6
Anthony Basko <antony.basko@gmail.com>
2025-02-09 02:24:27 -0500

    Player Movement & Main Mechanics
    Player dies when bots touch player
    Player can now reach the end

---
+++ Assets/Scripts/Coffee.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Coffee : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        rb.linearVelocity = (new Vector3(x, y) * Time.deltaTime).normalized * speed;
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Evil") {
+            Destroy(gameObject);
+        }
+
+        if (other.tag == "Coffee") {
+            
+        }
+    }
+}
